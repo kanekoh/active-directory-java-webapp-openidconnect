55247b9d2a99491d71b0c5057e6bb4119d506a1b
Hiroyuki Kaneko <hkaneko@redhat.com>
2019-11-26 17:57:56 +0900

    Set parameter from env

--- src/main/java/com/microsoft/aad/adal4jsample/BasicFilter.java
+++ src/main/java/com/microsoft/aad/adal4jsample/BasicFilter.java
@@ -305,13 +305,16 @@ public class BasicFilter implements Filter {
     }
 
     public void init(FilterConfig config) throws ServletException {
-        clientId = config.getInitParameter("client_id");
+        // clientId = config.getInitParameter("client_id");
+        clientId = System.getenv("AD_CLIENT_ID");
         logger.info("clientId " + clientId);
-        authority = config.getServletContext().getInitParameter("authority");
+        // authority = config.getServletContext().getInitParameter("authority");
         logger.info("authority " + authority);
-        tenant = config.getServletContext().getInitParameter("tenant");
+        // tenant = config.getServletContext().getInitParameter("tenant");
+        tenant = System.getenv("AD_TENANT");
         logger.info("tenant " + tenant);
-        clientSecret = config.getInitParameter("secret_key");
+        // clientSecret = config.getInitParameter("secret_key");
+        clientSecret = System.getenv("AD_SECRET_KEY");
         logger.info("clientSecret " + clientSecret);
 
     }
